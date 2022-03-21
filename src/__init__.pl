50480a2261dc1aab8b7623012ea79244f6cc4c37
Carlos Hernandez Corbato <c.h.corbato@tudelft.nl>
2022-03-21 11:28:59 +0100

    fixed change in knowrob structure db/tripledb

--- src/__init__.pl
+++ src/__init__.pl
@@ -1,6 +1,3 @@
 % add prolog modules that you want to load when starting the prolog package
 :- use_module(library('my_module')).
 
-% load library to use the mongoDB to load and access OWL ontologies from prolog
-:- use_module(library('db/tripledb'),
-    [ tripledb_load/2 ]).
