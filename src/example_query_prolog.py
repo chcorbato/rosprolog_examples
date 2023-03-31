7f3fd082d00d4524829f9b4b801addd0c0509f6c
chernandezcorb <c.h.corbato@tudelft.nl>
2023-03-31 11:42:01 +0200

    fixed for noetic python3

--- src/example_query_prolog.py
+++ src/example_query_prolog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import roslib; roslib.load_manifest('rosprolog')
 
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     prolog = Prolog()
     query = prolog.query("perishable(X), location(X,Y,_)")
     for solution in query.solutions():
-        print 'Found solution. Product: %s, Location: %s' % (solution['X'], solution['Y'])
+        print(("Found solution. Product: {}, Location: {}").format(solution['X'], solution['Y']))
     query.finish()
